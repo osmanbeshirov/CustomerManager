1f1729a66d4eb77bb05d2428b3a77de223689147
osmanbeshirov <osman.beshirov@gmail.com>
2021-11-15 15:26:58 +0400

    CustomerManager
    Musterilerin listelenmesi

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void addToList(Customer musteri)
+        {
+            Console.WriteLine(musteri.CustomerName + musteri.CustomerSurname + "adli muşterinin serveti: " + musteri.CustomerWealth  ); 
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.CustomerId = 1;
+            customer1.CustomerName = "  Osman";
+            customer1.CustomerSurname = "  Beshirov  ";
+            customer1.CustomerWealth = 200000;
+
+            Customer customer2 = new Customer();
+            customer2.CustomerId = 2;
+            customer2.CustomerName = "  Kanan";
+            customer2.CustomerSurname = "  Akhmadzade  ";
+            customer2.CustomerWealth = 500000;
+
+            Customer customer3 = new Customer();
+            customer3.CustomerId = 3;
+            customer3.CustomerName = "  Maqqa";
+            customer3.CustomerSurname = "  Aliyev  ";
+            customer3.CustomerWealth = 4545400000;
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+            CustomerManager customerManager = new CustomerManager();
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.CustomerId + customer.CustomerName + customer.CustomerSurname + customer.CustomerWealth);
+                //customerManager.addToList(customer);
+            }
+
+            
+           // customerManager.addToList(customer1);
+
+        }
+    }
+}
